289272a7389b64da51d0ffd85889349b6e566085
Monica <maaylahuerta@gmail.com>
2024-01-21 11:45:28 -0300

    Error existete en  metodo put

--- index.js
+++ index.js
@@ -23,9 +23,18 @@ app.post('/canciones', (req,res) => {
     const cancion = req.body;
     const canciones = JSON.parse(fs.readFileSync('canciones.json', 'utf-8'));
     canciones.push(cancion);
-    console.log(canciones)
     fs.writeFileSync('canciones.json', JSON.stringify(canciones));
     res.send('Canción agregada con éxito!');
+});
+
+app.put('/canciones/:id', (req,res) => {
+    const {id} = req.params;
+    const cancion = req.body;
+    const canciones = JSON.parse(fs.readFileSync('canciones.json','utf-8'));
+    const indice = canciones.findIndex((item) => item.id == id);
+    canciones[indice] = cancion;
+    fs.writeFileSync('canciones.json',JSON.stringify(canciones));
+    res.send('Canción modificada con éxito');
 });
 
 app.delete('/canciones/:id', (req,res) => {
