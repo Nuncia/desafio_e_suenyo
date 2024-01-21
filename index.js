44f0dbad9189a0e5907be89ddef71b1a85643ffc
Monica <maaylahuerta@gmail.com>
2024-01-21 11:33:53 -0300

    Error metodo put

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const routes = require('./routes/routes');
+// const routes = require('./routes/routes');
 const fs = require('fs');
 
 const PORT = process.env.PORT || 3010;
@@ -14,14 +14,28 @@ app.get('/', (req,res) => {
     res.sendFile(__dirname + "/index.html");  
 });
 
+app.get('/canciones', (req,res) => {
+    const canciones = JSON.parse(fs.readFileSync('canciones.json', 'utf-8'));
+    res.json(canciones);
+});
+
 app.post('/canciones', (req,res) => {
     const cancion = req.body;
     const canciones = JSON.parse(fs.readFileSync('canciones.json', 'utf-8'));
-    console.log('cancion: ', cancion)
     canciones.push(cancion);
+    console.log(canciones)
     fs.writeFileSync('canciones.json', JSON.stringify(canciones));
     res.send('Canción agregada con éxito!');
-})
+});
+
+app.delete('/canciones/:id', (req,res) => {
+    const {id} = req.params;
+    const canciones = JSON.parse(fs.readFileSync('canciones.json','utf-8'));
+    const indice = canciones.findIndex((item) => item.id == id);
+    canciones.splice(indice,1);
+    fs.writeFileSync('canciones.json',JSON.stringify(canciones));
+    res.send('Canción eliminada con éxito');
+});
 
 app.listen(PORT, () => console.log(`Escuchando por el puerto ${PORT}`));
 
